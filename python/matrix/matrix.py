e86a01aaae706ab710a20621021b3b499c743163
Nick <nicholas.balestrieri@gmail.com>
2019-12-09 16:09:00 -0500

    add solution for matrix problem

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,20 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.rows = matrix_string.split("\n")
+
+        for str in range(len(self.rows)):
+            self.rows[str] = self.rows[str].split(" ")
+            for i in range(len(self.rows[str])):
+                self.rows[str][i] = int(self.rows[str][i])
+
+        self.columns = []
+
+        for each in range(len(self.rows[0])):
+            self.columns.append([row[each] for row in self.rows])
+            
 
     def row(self, index):
-        pass
+        return self.rows[index-1]
 
     def column(self, index):
-        pass
+        return self.columns[index-1]
