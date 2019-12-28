66f02fd8c5954b3d0734f6ebffd9ebf8ca705903
Nick <nicholas.balestrieri@gmail.com>
2019-12-28 15:15:54 -0500

    Remove unnecessary second check for empty string

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -2,8 +2,7 @@ from functools import reduce
 import re
 
 def return_capitalized_first_letter(word):
-    if len(word) > 0:
-        return word[0].upper()
+    return word[0].upper()
 
 def abbreviate(words):
     regex = re.compile("[\\s\\-\\_]")
